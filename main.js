8352785bf502ed9370e3f71f7e764dd31a5de6fc
elinabhatt <74358269+elinabhatt@users.noreply.github.com>
2021-06-03 00:26:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ function preload(){
 function setup(){
   canvas = createCanvas(300, 300);
   canvas.center();
+  video=createCapture(VIDEO);
+  video.size(300,300)
+  video.hide();
+
+  poseNet = ml5.poseNET(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+  console.log('PoseNet Is Intialized');
 }
 
 function draw(){
